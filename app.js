fb98730865908ac5e5c7868a4d7d03a75100bccc
Pedro Marchetti Casteli <pedro.casteli@zaffarinet.com.br>
2022-08-18 14:20:44 -0300

    Implementing GET and POST routes.

--- app.js
+++ app.js
@@ -1,21 +1,47 @@
+import fs from "fs";
 import express from "express";
 import "dotenv/config";
 
 const app = express();
 
-app.get("/", (req, res) => {
+// This is called a MIDDLEWARE, because it goes BETWEEN the request and the response. This middleware in particular, adds the body data to the request object.
+app.use(express.json());
+
+// app.get("/", (req, res) => {
+//     res.status(200).json({
+//         status: "success",
+//         message: "You can GET to this endpoint...",
+//         app: "Batman-API",
+//     });
+// });
+
+// app.post("/", (req, res) => {
+//     res.status(200).json({
+//         status: "success",
+//         message: "You can POST to this endpoint...",
+//         app: "Batman-API",
+//     });
+// });
+
+const heroes = JSON.parse(fs.readFileSync("./dev-data/data/heroes.json"));
+
+// This callback function is called a ROUTE HANDLER
+app.get("/api/v1/heroes", (req, res) => {
     res.status(200).json({
         status: "success",
-        message: "You can GET to this endpoint...",
-        app: "Batman-API",
+        results: heroes.length,
+        data: { heroes: heroes },
     });
 });
 
-app.post("/", (req, res) => {
-    res.status(200).json({
-        status: "success",
-        message: "You can POST to this endpoint...",
-        app: "Batman-API",
+app.post("/api/v1/heroes", (req, res) => {
+    const newId = heroes[heroes.length - 1].id + 1;
+    const newHero = Object.assign({ id: newId }, req.body);
+
+    heroes.push(newHero);
+
+    fs.writeFile("./dev-data/data/heroes.json", JSON.stringify(heroes), (e) => {
+        res.status(201).json({ status: "success", data: { hero: newHero } });
     });
 });
 
