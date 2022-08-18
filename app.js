3a936d6eefa2d5d78afa3cd3704359d6ea11f8a4
Pedro Marchetti Casteli <pedro.casteli@zaffarinet.com.br>
2022-08-18 14:52:53 -0300

    CRUD operations implemented.

--- app.js
+++ app.js
@@ -34,6 +34,19 @@ app.get("/api/v1/heroes", (req, res) => {
     });
 });
 
+app.get("/api/v1/heroes/:id", (req, res) => {
+    const hero = heroes.find((el) => el.id === req.params.id * 1);
+
+    if (!hero) {
+        return res.status(404).json({ status: "fail", message: "Invalid ID" });
+    }
+
+    res.status(200).json({
+        status: "success",
+        data: { hero: hero },
+    });
+});
+
 app.post("/api/v1/heroes", (req, res) => {
     const newId = heroes[heroes.length - 1].id + 1;
     const newHero = Object.assign({ id: newId }, req.body);
@@ -42,6 +55,30 @@ app.post("/api/v1/heroes", (req, res) => {
 
     fs.writeFile("./dev-data/data/heroes.json", JSON.stringify(heroes), (e) => {
         res.status(201).json({ status: "success", data: { hero: newHero } });
+    });
+});
+
+app.patch("/api/v1/heroes/:id", (req, res) => {
+    if (req.params.id * 1 > heroes.length) {
+        return res.status(404).json({ status: "fail", message: "Invalid ID" });
+    }
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            hero: "<Updated hero here>",
+        },
+    });
+});
+
+app.delete("/api/v1/heroes/:id", (req, res) => {
+    if (req.params.id * 1 > heroes.length) {
+        return res.status(404).json({ status: "fail", message: "Invalid ID" });
+    }
+
+    res.status(204).json({
+        status: "success",
+        data: null,
     });
 });
 
