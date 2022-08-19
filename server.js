291862341538d7ac92ec496538c45f749e5e743b
Pedro Marchetti Casteli <pedro.casteli@zaffarinet.com.br>
2022-08-19 10:21:15 -0300

    Connecting to our MongoDB database using Mongoose.

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
-import app from "./app.js";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import app from "./app.js";
 
 dotenv.config({ path: "./.env" });
+
+const DB = process.env.DATABASE.replace(
+    "<password>",
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+    })
+    .then((con) => console.log("DB connection successful!"));
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
