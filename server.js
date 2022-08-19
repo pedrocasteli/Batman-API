d0508d38ed2f99da2a0e719d1d77911daffcf50d
Pedro Marchetti Casteli <pedro.casteli@zaffarinet.com.br>
2022-08-19 11:05:52 -0300

    Creating and saving our first document into the 'heroes' collection in our database.

--- server.js
+++ server.js
@@ -15,6 +15,40 @@ mongoose
     })
     .then((con) => console.log("DB connection successful!"));
 
+const heroSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [
+            true,
+            "How can a hero exist without a name that strikes fear into the hearts of enemies?",
+        ],
+        unique: true,
+    },
+    real_name: {
+        type: String,
+        required: [
+            true,
+            "Human or otherwise, it's gotta have an actual name, right?",
+        ],
+    },
+    gender: { type: String, default: "Unknown/Irrelevant" },
+    base_of_operation: { type: String, default: "Unknown/Irrelevant" },
+});
+
+const Hero = mongoose.model("Heroes", heroSchema);
+
+const heroDocument = new Hero({
+    name: "Nightwing",
+    real_name: "Dick Grayson",
+    gender: "male",
+    base_of_operation: "Bludhaven",
+});
+
+heroDocument
+    .save()
+    .then()
+    .catch((e) => console.log(e));
+
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
